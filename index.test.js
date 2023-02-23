01765b8d3827e075012c64fa722bb47d30e728a5
Hannah <115044113+hannahnobeebux@users.noreply.github.com>
2023-02-23 12:46:20 +0000

    completed tasks

--- index.test.js
+++ index.test.js
@@ -6,6 +6,7 @@ const app = require('./index');
 const { sequelize, Dog } = require('./db');
 const seed = require('./db/seedFn');
 const {dogs} = require('./db/seedData');
+const supertest = require('supertest');
 
 describe('Endpoints', () => {
     // to be used in POST test
@@ -33,4 +34,34 @@ describe('Endpoints', () => {
             expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
         });
     });
+
+    describe('POST /dogs', () => {
+        it('should add a new dog to the database', async  () => {
+            const dogData = {name: "Max", breed: "Labrador Retriever", color: "Golden", description:"A friendly and loyal dog"}
+            const response = await request(app).post('/dogs').send(dogData)
+            expect(response.status).toBe(200)
+
+
+        })
+        it('should match the data saved in the database', async () => {
+            const dogFromDatabase = Dog.findOne({where: {name: "Max"}})
+            expect(dogFromDatabase).toBeDefined()
+        })
+    })
+
+    describe('DELETE /dogs/:id', () => {
+        it('should delete the dog with an id of 1', async () => {
+            const response = await request(app).delete('/dogs/1')
+            const dog = Dog.findAll({where: id = 1})
+            expect(typeof response.body).toBe("object");
+        })
+    })
+
+    describe('GET /dogs/:name', () => {
+        it('should return the dog with the corresponding name', async () => {
+            const response = await request(app).get('/dogs/:name')
+            const dog = Dog.findByPk(response.body.name)
+            expect(dog).toBeDefined; 
+        })
+    })
 });
